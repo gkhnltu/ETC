0f9cb17a74a432ae9116934a478b463f99653345
unknown <894876592@qq.com>
2020-09-14 21:25:23 +0800

    9.14

---
+++ 9.14王忠图/gr今天的代码.java
@@ -0,0 +1,185 @@
+package test;
+import java.util.Scanner;
+public class gr今天的代码 {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		//public表示公有权限
+		//static静态修饰，没有对象好可以调用静态方法
+		//main程度的入口
+		//String是字符串类
+//		Scanner scan=new Scanner(System.in);
+//		byte a=20;
+//		short b=5000;
+//		int c=234243;
+//		long d=123456789;
+//		float f=20.222222f;
+//		double d1=20.22222;
+//		System.out.println(f);
+//		System.out.println(d1);
+//		char g='a';
+//	
+//		String nameString="maker";
+////		
+//		byte a=10;
+//		System.out.println(a);
+//		short b=a;
+//		System.out.println(b);
+//		int c=b;
+//		System.out.println(c);
+//		float f=c;
+//		System.out.println(f);
+//		double d=f;
+//		System.out.println(d);
+//		
+//		double d1=20.22222;
+//		float f1=(float)d1;
+//		int c1=42;
+//		short b1=(short)c1;
+//		System.out.println(b1);
+//		byte a2=(byte)1000;
+//		System.out.println(a2);
+//		
+//				
+//		int a=10;
+//		int b=20;
+//		int c=a+b;
+//		int c1=a-b;
+//		int c2=a*b;
+//		int c3=a/b;
+//		int c4=a%b;
+//		System.out.println(c);
+//		System.out.println(c1);
+//		System.out.println(c2);
+//		System.out.println(c3);
+//		System.out.println(c4);
+//		
+//		int x=10;
+//		x-=2;
+//		x+=2;
+//		x*=2;
+//		x/=2;
+//		x%=3;
+//		System.out.println(x);
+//				
+//		int y=20;
+//		int v=10;
+//		System.out.println(y==v);
+//		System.out.println(y>v);
+//		System.out.println(y<v);
+//		System.out.println(y!=v);
+//		System.out.println(y>=v);
+//		System.out.println(y<=v);
+//		
+//		boolean k=true;
+//		boolean kk=false;
+//		System.out.println(k&&kk);
+//		System.out.println(k&kk);
+//		System.out.println(k|kk);
+//		System.out.println(k||kk);
+//		System.out.println(k^kk);
+//		System.out.println(!kk);
+//		
+//		
+//		int i=scan.nextInt();
+//		System.out.println(i);
+//		float f=scan.nextFloat();
+//		float f1=20.22f;
+//		System.out.println(f);
+//		String string1=scan.next();
+//		System.out.println(string1);
+//		String string2=scan.nextLine();
+//		System.out.println(string2);
+//		System.out.println("结束");
+		
+		
+//		int a=10;
+//		int b=20;
+//		int c=a>b?a:b;
+//		System.out.println(c);
+//		
+//	Scanner scanner=new Scanner(System.in);
+//
+//		System.out.println("1");
+//		int a1=scanner.nextInt();
+//		System.out.println("2");
+//		int a2=scanner.nextInt();
+//		System.out.println("3");
+//		int a3=scanner.nextInt();
+//		
+//		int b2=a1>a2?(a1>a3?a1:a3):(a2>a3?a2:a3);
+//		System.out.print(b2);
+		
+		
+//		boolean b=true;
+//		if(b) {
+//			System.out.println("我是if");
+//		}
+//		
+//		int a=30;
+//		int c=20;
+//		if(a>c) {
+//			System.out.println("a>b");
+//		}
+//		if(100>20) {
+//			System.out.println("100>20");
+//		}
+//		
+//		
+//		Scanner scanner=new Scanner(System.in);
+//		int a1=scanner.nextInt();
+//		if(a1>10) {
+//			System.out.println(">10");
+//		}
+		
+		
+		
+//	Scanner scanner=new Scanner(System.in);
+//	int a1=scanner.nextInt();
+//	int a2=scanner.nextInt();
+//	int a3=scanner.nextInt();
+//	if(a1>a2) {
+//		if(a1>a3) {
+//			System.out.println(a1);
+//		}else {
+//			System.out.println(a3);
+//		}
+//	}else {
+//		if(a2>a3) {
+//			System.out.println(a2);
+//		}else {
+//			System.out .println(a3);
+//		}
+//	}
+//		
+		
+		
+			
+			Scanner scan=new Scanner(System.in);
+			int a=scan.nextInt();
+			if(a>=1 && a<=3){
+				System.out.println("春季");
+			}else if(a>=4 && a<=6){
+				System.out.println("夏季");
+			}else if(a>=7 && a<=9){
+				System.out.println("秋季");
+			}else if(a>=10 && a<=12){
+				System.out.println("冬季");
+			}else {
+				System.out.println("数据不正确");
+			}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
